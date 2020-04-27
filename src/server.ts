5a158d403920bdf2a1749a75b78e2046c8bcb74b
Tetevi Placide Ekon <ekontetevi@yahoo.fr>
2020-04-27 10:13:25 +0000

    Add files via upload

---
+++ src/server.ts
@@ -0,0 +1,72 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { filterImage, deleteLocalFiles } from './util/util';
+import { Magic, MAGIC_MIME_TYPE } from 'mmmagic';
+import Jimp from 'jimp';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  app.get('/filteredimage', async (req, res) => {
+    const url: string = req.query.image_url;
+    
+    if (!url) {
+      return res.status(400).send('Image url must be provided!');
+    }
+
+    const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    const regex = new RegExp(expression);
+
+    if (!url.match(regex)) {
+      return res.status(400).send('Url is not valid!');
+    }
+
+    const fileFromUrl = await Jimp.read(url);
+    const fileMIME = fileFromUrl.getMIME();
+    
+    if (!fileMIME.includes('image')) {
+      return res.status(400).send('Url does not contain an image!');
+    }
+
+    return res.status(200).sendFile(await filterImage(fileFromUrl));
+  });
+
+  //! END @TODO1
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  });
+
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
