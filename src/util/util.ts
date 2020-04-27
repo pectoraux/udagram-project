a7a61f8ca0833b371c8d0525e32e60b4b345a785
Tetevi Placide Ekon <ekontetevi@gmail.com>
2020-04-27 10:12:47 +0000

    Create util.ts

---
+++ src/util/util.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import Jimp from 'jimp';
+
+// filterImageFromURL
+// helper function to download, filter, and save the filtered image locally
+// returns the absolute path to the local image
+// INPUTS
+//    inputURL: string - a publicly accessible url to an image file
+// RETURNS
+//    an absolute path to a filtered image locally saved file
+export async function filterImage(image: any): Promise<string>{
+    return new Promise( async resolve => {
+        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
+        await image
+        .resize(256, 256) // resize
+        .quality(60) // set JPEG quality
+        .greyscale() // set greyscale
+        .write(__dirname+outpath, (img: any)=>{
+            resolve(__dirname+outpath);
+        });
+    });
+}
+
+// deleteLocalFiles
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+export async function deleteLocalFiles(files:Array<string>){
+    for( let file of files) {
+        fs.unlinkSync(file);
+    }
+}
